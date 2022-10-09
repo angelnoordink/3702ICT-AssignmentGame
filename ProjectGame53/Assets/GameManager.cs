80c24540af38ac08eeb8034e64ff0ec2daa58cc9
josh-thompson13 <69380003+josh-thompson13@users.noreply.github.com>
2022-10-09 14:50:40 +1000

    Fixed timer issue

--- ProjectGame53/Assets/GameManager.cs
+++ ProjectGame53/Assets/GameManager.cs
@@ -15,7 +15,6 @@ public class GameManager : MonoBehaviour {
 
     [SerializeField] public EndTimer endTimer;
 
-    public GameObject TimerHandler;
 
     public GameObject ThirdPersonController;
 
@@ -33,7 +32,7 @@ public class GameManager : MonoBehaviour {
         }
         else if(miniGameCountSO.minigame_count == 3){
 
-            endTimer.timer = TimerHandler.timer;
+           endTimer.timer = GameObject.Find("TimerHandler").GetComponent<TimerHandler>().timer;
             if(endTimer.timer > 0){
                 SceneManager.LoadScene("WinOutcome");
             }
